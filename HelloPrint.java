4d852787029545e20bbfc37939604adeb3fa9ab4
dhanlakshmi123 <dhanlakshmihedgire@gmail.com>
2023-03-07 10:52:38 +0530

    HelloWorld-first commit

---
+++ HelloPrint.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class HelloPrint 
+{
+	public String HelloPrint()
+	{
+		return "Hello World";
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		HelloPrint hello = new HelloPrint();
+		
+		System.out.println(hello.HelloPrint());
+
+	}
+
+}
